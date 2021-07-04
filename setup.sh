548a224d185d10a83e257c2f4815206b9e379dd5
Netdea <79692363+Netdea@users.noreply.github.com>
2021-07-04 16:12:36 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-$apt update && upgrade -y
-$pkg install figlet -y
-$apt install python -y
-$pkg install python -y
-$apt install python3 -y
-$pip install tqdm -y
-$pip install colorama -y
-$pip install phonenumbers -y
-$pip install geocoder -y
-$pip install folium -y
-$pip install sock -y
-$pip install random -y
-$pip install bs4
-$pip install Bea
+apt update && upgrade -y
+pkg install figlet -y
+apt install python -y
+pkg install python -y
+apt install python3 -y
+pip install tqdm -y
+pip install colorama -y
+pip install phonenumbers -y
+pip install geocoder -y
+pip install folium -y
+pip install sock -y
+pip install random -y
+pip install bs4
+pip install Beautifulsoup
