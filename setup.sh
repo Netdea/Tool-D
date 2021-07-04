86ebabef66a2eb97f8c77ce118c62a9b67a7b086
Netdea <79692363+Netdea@users.noreply.github.com>
2021-07-04 16:17:49 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@ pkg install figlet -y
 apt install python -y
 pkg install python -y
 apt install python3 -y
-pip install tqdm -y
-pip install colorama -y
-pip install phonenumbers -y
-pip install geocoder -y
-pip install folium -y
-pip install sock -y
-pip install random -y
+pip install tqdm 
+pip install colorama 
+pip install phonenumbers 
+pip install geocoder 
+pip install folium 
+pip install sock 
+pip install random 
 pip install bs4
 pip install Beautifulsoup
